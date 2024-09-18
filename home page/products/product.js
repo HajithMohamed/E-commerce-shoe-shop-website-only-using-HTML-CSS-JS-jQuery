52d9924ad6052f43e69d2316f5615c5ab1520238
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2024-09-18 20:01:48 +0530

    order page changes

--- home page/products/product.js
+++ home page/products/product.js
@@ -1,3 +1,5 @@
+
+
 function showSlideBar(){
     const side_items=document.querySelector(".side_items")
     side_items.style.display="flex"
@@ -9,14 +11,14 @@ function hideSlideBar(){
 
     function nullCheck(){
         const msg= document.getElementById("error")
-        const button=document.querySelector("#btn")
+        const button3=document.querySelector("#btn")
         let error="Please fill this"
         const mailVal=document.getElementById("mail").value;
         if (!mailVal){
            msg.innerHTML=error;
            //console.log(msg);    
         } 
-        button.addEventListener("click",()=>{
+        button3.addEventListener("click",()=>{
                 if (!mailVal){
                     msg.innerHTML="E-mail is required"
                 }
@@ -28,8 +30,8 @@ function hideSlideBar(){
                 }
             }) 
     }
-    const button=document.querySelector("#btn")
-    button.addEventListener("submit",()=>{
+    const button1=document.querySelector("#btn")
+    button1.addEventListener("submit",()=>{
         const mailVal=document.querySelector("#mail").value;
         const msg= document.getElementById("error")
         if (!mailVal){
@@ -50,94 +52,40 @@ const validateEmail = (email) => {
       );
   };
 
-  function stockAmount(productId) {
+  function stockAmount1(productId) {
+    let productStock =0;
+    let carded_stock=0;
+    const initial_stock=parseInt(document.getElementById(`product${productId}`).value)
     const popUp = document.getElementById(`pop-up${productId}`);
-    const span = document.getElementById(`product${productId}`);
-    const qty = parseInt(span.innerHTML);
-    let value = qty - 1;
+    productStock = initial_stock-1;
+    carded_stock=initial_stock-productStock;
 
-    if (value >= 0) {
-        let initialStock=20;
-        document.getElementById(`pop-up-span${productId}`).innerHTML = initialStock - value;
-        popUp.style.display="block";
-        
-    }
+    document.getElementById(`pop-up-span${productId}`).innerHTML =  carded_stock;
 
-    if (value <= 0) {
-        alert("This product is out of stock");
-    }
     
+    popUp.style.display = "block"; 
 }
-
-function closePopUp(productId) {
+function closePopUp1(productId) {
     const popUp = document.getElementById(`pop-up${productId}`);
     popUp.style.display="none";
 }
 
-//order products page's css
-
-function nullCheck(){
-    const msg= document.getElementById("error")
-    const button=document.querySelector("#btn")
-    let error="Please fill this"
-    const mailVal=document.getElementById("mail").value;
-    if (!mailVal){
-       msg.innerHTML=error;    
-    } 
-    button.addEventListener("click",()=>{
-            if (!mailVal){
-                msg.innerHTML="E-mail is required"
-            }
-            else if(!validateEmail(mailVal)) {
-                msg.innerHTML="Pls enter the valid Email"
-            }
-            else{
-                alert("You have successfully subscribed");
-            }
-        }) 
-}
-const button2=document.querySelector("#btn")
-
-
-console.log(button);
-button2.addEventListener("click",()=>{
-    const mailVal=document.querySelector("#mail").value;
-    const msg= document.getElementById("error")
-    if (!mailVal){
-        msg.innerHTML="E-mail is required"
-    }
-    else if(!validateEmail2(mailVal)) {
-        msg.innerHTML="Pls enter the valid Email"
-    }
-    else{
-        alert("You have successfully subscribed");
-    }
-}) 
-const validateEmail2 = (email) => {
-return String(email)
-  .toLowerCase()
-  .match(
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
-};
+//order products page's js
 const form=document.getElementById("form")
 const qty=document.getElementById("qty")
 const mobile=document.getElementById("mobile")
 const address=document.getElementById("address")
 const size=document.querySelector("#size")
 
-function oredersucces(event){
-
-if (!validateForm()) {
-   event.preventDefault();
-   
-}
-else{
-    stockAmount();  
-}
-}
-
-
+form.addEventListener("Submit",(event)=>{
+    if (!validateForm()) {
+        event.preventDefault();
+        
+     }
+     else{
+         stockAmount();  
+     }
+})
 
 function validateForm(){
   let isValid = true;
@@ -184,9 +132,6 @@ if(sizeVal==="Select the size:"){
   return isValid
 }
   
-  
-
-
 function setError(element,message){
   const Element=element.parentElement
   const errorElement=Element.querySelector(".error")
@@ -207,37 +152,33 @@ function setSuccess(element){
   stockAmount();
 
 }
-function stockAmount() {
+function stockAmount() {        
+    const input = document.querySelector("#size");
+    const popUp = document.getElementById(`pop-up`);
+    const span1 = document.getElementById("pop-up-span1");
+    const span2 = document.querySelector("#si");
+    const span3 = document.querySelector("#price");
+    const span4 = document.querySelector("#Payment");
     
-  const input=document.querySelector("#size")
-  const popUp = document.getElementById(`pop-up`);
-  const span1=document.getElementById("pop-up-span1")
-  const span2=document.querySelector("#si")
-  const span3=document.querySelector("#price")
-  const span4=document.querySelector("#Payment")
+    const card = document.querySelector("#card");
+    const cash = document.querySelector("#cash");
+    
+    const sizeVal = input.options[input.selectedIndex].value;
+    const qtyVal = document.getElementById("qty").value;
+    
+    if (card.checked) {
+        span4.innerHTML = "Card";
+    } else if (cash.checked) {
+        span4.innerHTML = "Cash on delivery";
+    }
 
-  const card=document.querySelector("#card")
-  const cash=document.querySelector("#cash")
-
-  const sizeval=input.options[input.selectedIndex].value
-  const qtyVal=qty.value
-  
-  if(card.checked){
-    span4.innerHTML="Card"
-  }
-  else if(cash.checked){
-    span4.innerHTML="Cash on delivery"
-  }
-  const price=qtyVal*15
-  span2.innerHTML=sizeval
-  span1.innerHTML = qtyVal;
-  span3.innerHTML = price;
-  popUp.style.visibility="visible";
-  popUp.style.transform="translate(120%,-130%)scale(1)";
+    const price = qtyVal * 15;
+    span2.innerHTML = sizeVal;
+    span1.innerHTML = qtyVal;
+    span3.innerHTML = price;
+    popUp.style.visibility = "visible";
+    popUp.style.transform = "translate(120%,-130%)scale(1)";
 }
-
-
-
 function closePopUp() {
   const popUp = document.getElementById(`pop-up`);
   popUp.style.display="none";
