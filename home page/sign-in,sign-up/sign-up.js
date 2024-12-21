c1069ab785b3b974b8f2281949ab7909e2e7d33a
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2024-12-21 12:23:14 +0530

    commit

--- home page/sign-in,sign-up/sign-up.js
+++ home page/sign-in,sign-up/sign-up.js
@@ -1,74 +1,71 @@
-const form=document.querySelector("#form")
+const form = document.querySelector("#form");
 
-form.addEventListener("submit",(event)=>{
-    if(!validateForm()){
-        event.preventDefault()
+form.addEventListener("submit", (event) => {
+    if (!validateForm()) {
+        event.preventDefault();
+    } else {
+        window.location.href = "sign-in.html";
     }
-    else{
-        window.location.href="sign-in.html"
-    }
-})
+});
 
-function validateForm(){
-    const user=document.querySelector("#name").value.trim()
-    const mail=document.querySelector("#mail").value.trim()
-    const pwd=document.querySelector("#pwd").value.trim()
-    const cpwd=document.querySelector("#cpwd").value.trim()
+function validateForm() {
+    const user = document.querySelector("#name").value.trim();
+    const mail = document.querySelector("#mail").value.trim();
+    const pwd = document.querySelector("#pwd").value.trim();
+    const cpwd = document.querySelector("#cpwd").value.trim();
 
-    let isValid=true
-    if (user == "" || mail =="" || pwd =="" || cpwd =="") {
-        setError("All fields should be filled")
-        isValid = false
+    let isValid = true;
+    if (user === "" || mail === "" || pwd === "" || cpwd === "") {
+        setError("All fields should be filled");
+        isValid = false;
+    } else if (!validateEmail(mail)) {
+        setError("Please enter a valid Email");
+        isValid = false;
     }
-    else if(!validateEmail(mail)){
-        setError("Please enter the valid Email")
-        isValid=false
+    if (pwd !== cpwd) {
+        setError("Passwords don't match!");
+        isValid = false;
     }
-    if(pwd!==cpwd){
-        setError("Password dosn't match!")
-        isValid=false
-    }
-    return isValid
+    return isValid;
 }
 
-function setError(message){
-    const error=document.querySelector(".error")
-    error.innerHTML=message
-    error.style.display="block"
-
+function setError(message) {
+    const error = document.querySelector(".error");
+    error.innerHTML = message;
+    error.style.display = "block";
 }
+
 const validateEmail = (email) => {
     return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 
-  const iconpwd=document.getElementById("icon-pwd")
-  const iconcpwd=document.getElementById("icon-cpwd")
+const iconpwd = document.getElementById("icon-pwd");
+const iconcpwd = document.getElementById("icon-cpwd");
 
-  iconpwd.addEventListener("click",()=>{
-    const pwd=document.querySelector("#pwd")
+iconpwd.addEventListener("click", () => {
+    const pwd = document.querySelector("#pwd");
 
-    if(pwd.type=="password"){
-        pwd.type="text"
-        iconpwd.src="eye-open.png"
+    if (pwd.type === "password") {
+        pwd.type = "text";
+        iconpwd.src = "eye-open.png";
+    } else {
+        pwd.type = "password";
+        iconpwd.src = "eye-close.png";
     }
-    else{
-        pwd.type="password"
-        iconpwd.src="eye-close.png"
-    }
-  })
-  iconcpwd.addEventListener("click",()=>{
-    const cpwd=document.querySelector("#cpwd")
+});
 
-    if(cpwd.type=="password"){
-        cpwd.type="text"
-        iconcpwd.src="eye-open.png"
+iconcpwd.addEventListener("click", () => {
+    const cpwd = document.querySelector("#cpwd");
+
+    if (cpwd.type === "password") {
+        cpwd.type = "text";
+        iconcpwd.src = "eye-open.png";
+    } else {
+        cpwd.type = "password";
+        iconcpwd.src = "eye-close.png";
     }
-    else{
-        cpwd.type="password"
-        iconcpwd.src="eye-close.png"
-    }
-  })
+});
